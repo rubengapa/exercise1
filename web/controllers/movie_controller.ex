7d821f6891e3edfebd569923199bc4dcdb7fd8e1
Ruben <ruben.garcia@bluetab.net>
2016-06-19 17:54:53 +0200

    Tercer test

--- web/controllers/movie_controller.ex
+++ web/controllers/movie_controller.ex
@@ -6,7 +6,11 @@ defmodule Ex1.MovieController do
 
   def index(conn, _params) do
     movieList = Movies.all
-    render  conn, "index.html", movieList: movieList     
+    render  conn, "index.html", movieList: movieList
+  end
+
+  def new(conn, _params) do
+    render  conn, "new.html"
   end
 
 end
