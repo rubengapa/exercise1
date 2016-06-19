24f78c4925056ae0af38a1a421d722cef139be00
Ruben <ruben.garcia@bluetab.net>
2016-06-19 17:38:48 +0200

    Primer test

---
+++ web/controllers/movie_controller.ex
@@ -0,0 +1,12 @@
+defmodule Ex1.MovieController do
+
+  use Ex1.Web, :controller
+
+  alias Ex1.Movies
+
+  def index(conn, _params) do
+    movieList = Movies.all
+    render  conn, "index.html", movieList: movieList     
+  end
+
+end
